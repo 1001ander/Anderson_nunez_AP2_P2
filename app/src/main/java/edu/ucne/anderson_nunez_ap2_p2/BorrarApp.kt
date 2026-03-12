dd89198be8198662223d27d116422b7d477057de
Anderson Nunez Consoro <an8634335@gmail.com>
2026-03-11 20:18:36 -0400

    Cambiar Nombres, a los archivos list.

--- app/src/main/java/edu/ucne/anderson_nunez_ap2_p2/BorrarApp.kt
+++
@@ -1,8 +0,0 @@
-package edu.ucne.anderson_nunez_ap2_p2
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class BorrarApp : Application()
-
--- app/src/main/java/edu/ucne/anderson_nunez_ap2_p2/presentation/list/BorrarListUiState.kt
+++
@@ -1,4 +0,0 @@
-package edu.ucne.anderson_nunez_ap2_p2.presentation.list
-
-class BorrarListUiState {
-}
